660ba298b0a17e2dbf81d5e6918eec193ae51395
Ali <noura.ali@vodafone.com>
2022-02-02 20:17:17 +0200

    video can be uploaded from anywhere

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+temp_folder_path = os.path.join( os.path.dirname(os.path.abspath(__file__)) , 'temp_folder' )
 #text2emotion = load_model()
 
 #module_url = 'https://tfhub.dev/google/universal-sentence-encoder/4'
@@ -23,10 +24,10 @@ app = Flask(__name__)
 @app.route('/' , methods=['POST', 'GET'])
 def index():
 	if request.method == 'POST':
-		video = request.form['video']
-		video2 = request.form['video2']
-		video3 = request.form['video3']
-		video4 = request.form['video4']
+		video = request.files['video']
+		video2 = request.files['video2']
+		video3 = request.files['video3']
+		video4 = request.files['video4']
 		name = request.form['name']
 		email = request.form['email']
 		answers = [
@@ -40,12 +41,18 @@ def index():
 		if not video2:
 			flash('please upload your answer first')
 		else:
-			#store video to GCS
-			video_list = [video,video2,video3,video4]
+			create_temp_dir()
 			create_output_files()
+
+			video_list = [video,video2,video3,video4]
+			
 			question_num=1
 			for i in range(len(video_list)):
-				upload_video_to_gcs(video_list[i],email,question_num)
+				video_path = get_video_path(video_list[i],question_num)
+				print(video_path)
+
+				#store video to GCS
+				upload_video_to_gcs(video_path,email,question_num)
 				##call emotion detection code --Mahmoud
 
 				#video_emotions,frames_timeline = FacialEmotionDetection(video,name,email,question_num)
@@ -58,9 +65,8 @@ def index():
 				3,a,a,1,0.44,0.09,0,0.08,0,0.39,0
 				"""
 				#get transript from the video
-				recognized_text = get_transcript(video_list[i])
-				
-
+				recognized_text = get_transcript(video_path)
+				print(recognized_text)
 				##detect transcript to emotions  --Shams
 				#result = text2emotion(recognized_text)
 				#emotion_str = str(result)
@@ -80,11 +86,10 @@ def index():
 				}
 				#rate = sentence_similarity(ans)
 				rate =  0.9
-				print(rate)
-				# rate =  0.9
 
 				append_to_output_files(email,name,question_num,rate,video_emotions,text_emotions,frames_timeline)
 				question_num += 1
+				remove_temp_video(video_path)
 
 			##upload results to GCS
 			upload_result_to_gcs(email)
@@ -95,22 +100,41 @@ def index():
 def response():
 	return render_template('review_response.html')
 
+
+def create_temp_dir():
+	isExist = os.path.exists(temp_folder_path)
+	if not isExist:
+		# Create a new directory because it does not exist 
+		os.mkdir(temp_folder_path, 0o777)
+		print("The temp directory is created!")
+	return 1
+
+def remove_temp_video(video_path):
+	os.remove(video_path)
+	return 1
+
+def get_video_path(video,question_num):
+	videofilename = "".join((str(question_num),"-",video.filename))
+	video.save(os.path.join( temp_folder_path , videofilename))
+	video_path = os.path.join( temp_folder_path , videofilename )
+	return video_path
+
 def create_output_files():
 	file1_header = "date,email,name,question_number,rate,ve_angry,ve_disgust,ve_fear,ve_happy,ve_sad,ve_surprise,ve_netural,te_Neutral,te_admiration,te_amusement,te_anger,te_annoyance,te_approval,te_caring,te_confusion,te_curiosity,te_desire,te_disappointment,te_disapproval,te_disgust,te_embarrassment,te_excitement,te_fear,te_gratitude,te_grief,te_joy,te_love,te_nervousness,te_optimism,te_pride,te_realization,te_relief,te_remorse,te_sadness,te_surprise"
 	#create file and write header to it
-	f = open("candidate_analysis.csv", "w")
+	f = open(os.path.join( temp_folder_path , "candidate_analysis.csv" ), "w")
 	f.write("".join((file1_header,"\n")) )
 	f.close()
 
 	file2_header = "frame_num,email,name,question_number,ve_angry,ve_disgust,ve_fear,ve_happy,ve_sad,ve_surprise,ve_netural"
 	#create file and write header to it
-	f = open("frames_timeline.csv", "w")
+	f = open( os.path.join( temp_folder_path , "frames_timeline.csv" ), "w")
 	f.write("".join((file2_header,"\n")) )
 	f.close()
 	return 1
 
 def append_to_output_files(email,name,question_num,rate,video_emotions,text_emotions,frames_timeline):
-	f = open("candidate_analysis.csv", "a")
+	f = open(os.path.join( temp_folder_path , "candidate_analysis.csv" ), "a")
 	now = datetime.now()
 	date = now.strftime("%d/%m/%Y %H:%M:%S")
 	question_num_str = str(question_num)
@@ -123,7 +147,7 @@ def append_to_output_files(email,name,question_num,rate,video_emotions,text_emot
 	f.write(record)
 	f.close()
 
-	f = open("frames_timeline.csv", "a")
+	f = open(os.path.join( temp_folder_path , "frames_timeline.csv" ), "a")
 	f.write(frames_timeline)
 	f.close()
 	return 1
@@ -133,10 +157,9 @@ def upload_video_to_gcs(video,applicant_mail,question_num):
 	# Setting credentials using the downloaded JSON file
 	client = storage.Client.from_service_account_json(json_credentials_path='hackathon-sa-credentials.json')
 	# Creating bucket object
-	bucket = client.get_bucket('hackalytics2')
+	bucket = client.get_bucket('hiring-application-bucket')
 	# Name of the destination file in the bucket
 	gcs_file_name = "".join(("applicants/videos/",applicant_mail,"_Q",str(question_num)))
-	print(gcs_file_name)
 	object_name_in_gcs_bucket = bucket.blob(gcs_file_name)
 	object_name_in_gcs_bucket.upload_from_filename(video)
 	return 1
@@ -147,46 +170,48 @@ def upload_result_to_gcs(applicant_mail):
 	# Setting credentials using the downloaded JSON file
 	client = storage.Client.from_service_account_json(json_credentials_path='hackathon-sa-credentials.json')
 	# Creating bucket object
-	bucket = client.get_bucket('hackalytics2')
+	bucket = client.get_bucket('hiring-application-bucket')
 	# Name of the destination file in the bucket
 	gcs_file_name = "".join(("applicants/results/",applicant_mail,"_candidate_analysis.csv"))
-	print(gcs_file_name)
 	object_name_in_gcs_bucket = bucket.blob(gcs_file_name)
-	object_name_in_gcs_bucket.upload_from_filename("candidate_analysis.csv")
+	object_name_in_gcs_bucket.upload_from_filename(os.path.join( temp_folder_path , "candidate_analysis.csv" ))
 
 	gcs_file_name2 = "".join(("applicants/frames_timeline/",applicant_mail,"_frames_timeline.csv"))
-	print(gcs_file_name)
 	object_name_in_gcs_bucket = bucket.blob(gcs_file_name2)
-	object_name_in_gcs_bucket.upload_from_filename("frames_timeline.csv")
+	object_name_in_gcs_bucket.upload_from_filename(os.path.join( temp_folder_path , "frames_timeline.csv" ))
 	return 1
 		
 
 #get transript from the video
 def get_transcript(video):
 	##convert video to audio  --Yara
-	VideoToAudio(video,"michael2.mp3")
+	VideoToAudio(video, os.path.join( temp_folder_path , "video_audio.mp3" ))
+
 	##call speech to text  --Yara
-	operation = AudioToText("michael2.mp3")
+	operation = AudioToText(os.path.join( temp_folder_path , "video_audio.mp3" ))
 
 	##transform text to get transcript 
 	recognized_text = ''
 	for i in range(len(operation.results)):    
 		recognized_text += operation.results[i].alternatives[0].transcript
-	print(recognized_text)
 	return recognized_text
 
 def VideoToAudio (VideoPath ,AudioPath ):
 	# Insert Local Video File Path
 	clip = mp.VideoFileClip(VideoPath)
 	clip.audio.write_audiofile(AudioPath)
+	clip.reader.close()
+	clip.close()
+	del clip.reader
+	del clip
 	return 1
 
-def AudioToText (VideoPath):
+def AudioToText (AudioPath):
 	os.environ['GOOGLE_APPLICATION_CREDENTIALS']='hackathon-sa-credentials.json'
 	speech_client = speech.SpeechClient()
-	media_file_name_mp3 = VideoPath
-	with open(media_file_name_mp3, 'rb') as f1:
+	with open(AudioPath, 'rb') as f1:
 		byte_data_mp3 = f1.read()
+		f1.close()
 	audio_mp3 = speech.RecognitionAudio(content=byte_data_mp3)
 
 	config_mp3 = speech.RecognitionConfig(
