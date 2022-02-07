2e4578f9782ba39df88e3ad20df65ba474f75bc0
nouraali3 <noura.ali9@yahoo.com>
2022-02-07 17:57:10 +0000

    fixed issue with text analysis file

--- text_emotions_analysis.py
+++ text_emotions_analysis.py
@@ -6,13 +6,13 @@ nltk.download('omw-1.4')
 
 
 def get_emotions(text):
-    emotions_result={}
-    result = te.get_emotion(text)
+	emotions_result={}
+	result = te.get_emotion(text)
 #     {'Happy': 0.33, 'Angry': 0.0, 'Surprise': 0.0, 'Sad': 0.0, 'Fear': 0.67}
 # {'friendly': 0.33, 'aggressive': 0.0, 'Surprise': 0.0, 'dissapointed': 0.0, 'hessitant': 0.67}
-    emotions_result['friendly']=result['Happy']
-    emotions_result['aggressive']=result['Angry']
-    emotions_result['suprised']=result['Surprise']
-    emotions_result['disappointed']=result['Sad']
-    emotions_result['hesitant']=result['Fear']
-    return emotions_result
+	emotions_result['friendly']=result['Happy']
+	emotions_result['aggressive']=result['Angry']
+	emotions_result['suprised']=result['Surprise']
+	emotions_result['disappointed']=result['Sad']
+	emotions_result['hesitant']=result['Fear']
+	return emotions_result
